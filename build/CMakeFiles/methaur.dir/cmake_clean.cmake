eec5ee190b9cb76889534fbb4c7c73b97582f64f
Kay <uhbonaparte@gmail.com>
2025-04-03 16:57:31 +0000

    Add files via upload

---
+++ build/CMakeFiles/methaur.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/methaur.dir/link.d"
+  "CMakeFiles/methaur.dir/src/methaur.c.o"
+  "CMakeFiles/methaur.dir/src/methaur.c.o.d"
+  "methaur"
+  "methaur.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/methaur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
